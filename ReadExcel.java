7c7fe515fb7be83e18d76001fb6fc6819cde1551
mayurk1993 <mayurk1993@gmail.com>
2019-09-06 16:41:49 +0530

    Update ReadExcel.java

--- ReadExcel.java
+++ ReadExcel.java
@@ -1,7 +1,9 @@
 package excel;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -15,7 +17,6 @@ public class ReadExcel {
 		// TODO Auto-generated method stub
 		try {
 			FileInputStream file = new FileInputStream(new File("C:\\Users\\mayur.kapoor\\eclipse-workspace\\Test2\\input.xlsx"));
-			
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
 			XSSFSheet sheet = workbook.getSheetAt(0);
 			
@@ -25,29 +26,37 @@ public class ReadExcel {
                 Row row = rowIterator.next();
                 //For each row, iterate through all the columns
                 Iterator<Cell> cellIterator = row.cellIterator();
-                 
+                BufferedWriter out = new BufferedWriter(new FileWriter("output.txt", true));  
                 while (cellIterator.hasNext())
                 {
                     Cell cell = cellIterator.next();
                     //Check the cell type and format accordingly
-                    System.out.print(cell.getCellType());
                     switch (cell.getCellType())
                     {
                         case Cell.CELL_TYPE_NUMERIC:
                             System.out.print(cell.getNumericCellValue() + "t");
+                            double a = cell.getNumericCellValue();
+                            String aa = a+",";
+                            
+                            		out.write(aa); 
+                            		
                             break;
                         case Cell.CELL_TYPE_STRING:
                             System.out.print(cell.getStringCellValue() + "t");
+                            String bb = cell.getStringCellValue()+",";
+                            
+                            out.write(bb);
                             break;
                     }
                 }
+                out.close();
                 System.out.println("");
             }
             file.close();
 		}
 		catch(Exception e)
 		{
-			System.out.println("Exception");
+			e.printStackTrace();
 		}
 	}
 
